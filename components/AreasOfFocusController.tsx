930d069a0a835f2007a319e238fd2346187f608c
eriksherman <38966637+eriksherman@users.noreply.github.com>
2022-11-07 20:02:50 -0500

    add instruction

--- components/AreasOfFocusController.tsx
+++ components/AreasOfFocusController.tsx
@@ -9,7 +9,8 @@ interface Props {
 }
 
 /**
- * Component that wraps AreasOfFocusSelector and manages the state
+ * Component that wraps AreasOfFocusSelector and manages the state so that users can select and deselect the areas of focus.
+ *
  * UI design: https://www.figma.com/file/f4PqVjtJnM8ECBHKQJrkgd/MeTimeVar1?node-id=1085%3A21205
  */
 export default function AreasOfFocusController({ all_areas_of_focus }: Props) {
